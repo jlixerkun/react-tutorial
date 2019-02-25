570e9f51716b27301e7af8bf6f448ec1ef44d7eb
jlixerkun <jlixerkun@gmail.com>
2019-02-24 22:37:38 -0500

    Updates

---
+++ precache-manifest.eb6116229cf064641ffe7b55ada458d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "84a702eb332f096cc3ad",
+    "url": "/react-tutorial/static/js/main.84a702eb.chunk.js"
+  },
+  {
+    "revision": "14bf863fa5da7fbc35b2",
+    "url": "/react-tutorial/static/js/2.14bf863f.chunk.js"
+  },
+  {
+    "revision": "84a702eb332f096cc3ad",
+    "url": "/react-tutorial/static/css/main.b6073034.chunk.css"
+  },
+  {
+    "revision": "1a8b4e80ab27b40a08b688c000e4cf13",
+    "url": "/react-tutorial/index.html"
+  }
+];
